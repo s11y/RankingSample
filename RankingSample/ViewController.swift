85881d362453371113dacb3744a3fe46c6472477
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-08-29 01:10:41 +0900

    Fix

--- RankingSample/ViewController.swift
+++ RankingSample/ViewController.swift
@@ -23,13 +23,17 @@ class ViewController: UIViewController {
 
     @IBOutlet var thirdLabel: UILabel!
 
-    let ref = Database.database().reference().child("ranking")
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
         self.fetch()
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,6 +49,7 @@ class ViewController: UIViewController {
 
     func send() {
 
+        let ref = Database.database().reference().child("ranking")
         let username: String = nameTextField.text!
         let score: Int = Int(scoreTextField.text ?? "0") ?? 0
 
@@ -53,12 +58,54 @@ class ViewController: UIViewController {
 
     func fetch() {
 
+        let ref = Database.database().reference().child("ranking")
         ref.queryOrdered(byChild: "score").queryLimited(toLast: 3).observe(.value, with: { snapshot in
-            
-            print(snapshot)
+
+            print("snap")
+
+            var snapshotArray: [DataSnapshot] = []
+
+            for item in snapshot.children {
+
+                snapshotArray.append(item as! DataSnapshot)
+            }
+
+            snapshotArray.reverse()
+
+            var rankArray: [Rank] = []
+
+            for snap in snapshotArray {
+
+                let rank = Rank(dic: snap.value as! [String: Any])
+                rankArray.append(rank)
+            }
+
+            for (index, rank) in rankArray.enumerated() {
+
+                if index == 0 {
+
+                    self.firstLabel.text = "\(rank.username)...\(rank.score)"
+                }else if index == 1 {
+
+                    self.secondLabel.text = "\(rank.username)...\(rank.score)"
+                }else if index == 2 {
+
+                    self.thirdLabel.text = "\(rank.username)...\(rank.score)"
+                }
+            }
         })
     }
-
-
 }
+
+class Rank {
+
+    var username: String = ""
+    var score: Int = 0
+
+    init(dic: [String: Any]) {
+
+        self.username = dic["username"] as? String ?? ""
+        self.score = dic["score"] as? Int ?? 0
+    }
+ }
 
