a779ecf00fb9135e3520a5d0fa4c3c614f485354
ShinokiRyosei <shinoki.r.0301@gmail.com>
2017-08-29 11:15:07 +0900

    Add comment

--- RankingSample/ViewController.swift
+++ RankingSample/ViewController.swift
@@ -13,26 +13,21 @@ import Firebase
 class ViewController: UIViewController {
 
 
-    @IBOutlet var scoreTextField: UITextField!
+    @IBOutlet var scoreTextField: UITextField! // スコア入力用のテキストフィールド
 
-    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var nameTextField: UITextField! // ユーザーネーム入力用のテキストフィールド
 
-    @IBOutlet var firstLabel: UILabel!
+    @IBOutlet var firstLabel: UILabel! // 1位表示用のラベル
 
-    @IBOutlet var secondLabel: UILabel!
+    @IBOutlet var secondLabel: UILabel! // 2位表示用のラベル
 
-    @IBOutlet var thirdLabel: UILabel!
+    @IBOutlet var thirdLabel: UILabel! // 3位表示用のラベル
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
         self.fetch()
-
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
 
     }
 
@@ -47,22 +42,26 @@ class ViewController: UIViewController {
     }
 
 
+    // スコア登録用メソッド
     func send() {
 
-        let ref = Database.database().reference().child("ranking")
-        let username: String = nameTextField.text!
-        let score: Int = Int(scoreTextField.text ?? "0") ?? 0
+        let ref = Database.database().reference().child("ranking") // 保存先を指定
+        let username: String = nameTextField.text! // ユーザーネーム
+        let score: Int = Int(scoreTextField.text ?? "0") ?? 0 // スコア
 
-        ref.childByAutoId().setValue(["username": username, "score": score])
+        ref.childByAutoId().setValue(["username": username, "score": score]) // 保存
     }
 
+    // 取得
     func fetch() {
 
-        let ref = Database.database().reference().child("ranking")
+        let ref = Database.database().reference().child("ranking") // 取得先を指定
+        // scoreの値が上から3つになるように設定して取得
         ref.queryOrdered(byChild: "score").queryLimited(toLast: 3).observe(.value, with: { snapshot in
 
             print("snap")
 
+            // DataSnapshotを配列に変換
             var snapshotArray: [DataSnapshot] = []
 
             for item in snapshot.children {
@@ -70,8 +69,10 @@ class ViewController: UIViewController {
                 snapshotArray.append(item as! DataSnapshot)
             }
 
+            // 順序が昇順なので、降順に
             snapshotArray.reverse()
 
+            // Snapshotの配列からRankの配列をつくる
             var rankArray: [Rank] = []
 
             for snap in snapshotArray {
@@ -80,6 +81,7 @@ class ViewController: UIViewController {
                 rankArray.append(rank)
             }
 
+            // Rankの配列を使ってラベルに表示
             for (index, rank) in rankArray.enumerated() {
 
                 if index == 0 {
@@ -97,6 +99,8 @@ class ViewController: UIViewController {
     }
 }
 
+
+// ランキングのデータをまとめるためのクラス
 class Rank {
 
     var username: String = ""
